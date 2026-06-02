24742fd7b5a296ccdc63026e449f6ca00ec90e22
Kallik <h.jarqueg@gmail.com>
2026-06-03 01:05:18 +0200

    [feature/add-endpoint-email]: add dependencies to the gradle

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -23,6 +23,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.boot:spring-boot-starter-mail")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-security")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
